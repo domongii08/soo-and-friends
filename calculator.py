2f9da484e199aa5036031c520e72be59fa6bc8e7
grassdoll3712-admin <cubejoon37@naver.com>
2024-05-29 18:58:57 +0900

    예외 처리 완

--- calculator.py
+++ calculator.py
@@ -2,8 +2,16 @@
 
 def main():
     #사칙연산에 사용할 두 수
-    a = 0
-    b = 0
+    while True:
+        try:
+            a = int(input('계산할 값 1을 입력하세요'))
+        except ValueError: print('올바른 값을 입력하세요.')
+        break
+    while True:
+        try:
+            b = int(input('계산할 값 2을 입력하세요'))
+        except ValueError: print('올바른 값을 입력하세요.')
+        break
 
     while True:
         operation = input("연산 방법을 선택하세요(0: 덧셈, 1: 뺄셈, 2: 나눗셈, 3: 곱셈):")
