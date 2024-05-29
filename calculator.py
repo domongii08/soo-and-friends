c152ed7164b479a59ac6176b2682a8edf6fd6abe
domongii08 <domongii08@hanmail.net>
2024-05-29 18:43:50 +0900

    initial commit

---
+++ calculator.py
@@ -0,0 +1,25 @@
+# 사칙연산 계산기 만들기
+
+def main():
+    a = 0
+    b = 0
+
+    while True:
+
+
+
+        if operation == '0':
+
+
+
+        elif operation == '1':
+
+
+
+        elif operation == "2":
+
+
+        else:
+
+if __name__ == '__main__':
+    main()
