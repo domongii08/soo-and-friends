a9d21dc10c4afbed385570fd80c1766fe746b614
grassdoll3712-admin <cubejoon37@naver.com>
2024-05-29 18:50:39 +0900

    operation 추가(연산 방법)

--- calculator.py
+++ calculator.py
@@ -1,11 +1,12 @@
 # 사칙연산 계산기 만들기
 
 def main():
+    #사칙연산에 사용할 두 수
     a = 0
     b = 0
 
     while True:
-
+        operation = input("연산 방법을 선택하세요(0: 덧셈, 1: 뺄셈, 2: 나눗셈, 3: 곱셈):")
 
 
         if operation == '0':
